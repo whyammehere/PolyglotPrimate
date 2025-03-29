3479b17ce4b23487faa99023d3157a0c5fbb0919
whyammehere <zhangian230791@gmail.com>
2025-03-29 16:57:24 -0400

    how does one get JS to work
    bruh

---
+++ scripts/translator.js
@@ -0,0 +1,10 @@
+console.log("TEST")
+//import * as deepl from 'deepl-node';
+
+//const authKey = ""; // REPLACE WITH ENVIRO VARIABLE
+//const translator = new deepl.Translator(authKey);
+
+//(async () => {
+//    const result = await translator.translateText('Hello, world!', null, 'fr');
+//    console.log(result.text); // Bonjour, le monde!
+//})();
